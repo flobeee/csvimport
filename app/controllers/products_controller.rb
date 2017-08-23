da8adf7e496942aaa87f7c2151aed10c5588277c
Matthew Wint <mattwint@Matt-Wint-MacBook-Pro.local>
2017-08-23 07:18:11 -0700

    added paging to the table

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,10 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.where(recordid: '302').paginate(:page => params[:page], :per_page => 10)
   end
 
   def import
-    Product.import(params[:file], params[:export], params[:site], params[:credentials], params[:object], params[:fieldname], params[:fieldto])
+    Product.import(params.require[:file], params[:export], params.require[:site], params.require[:credentials], params[:object], params[:fieldname], params[:fieldto])
     redirect_to root_url, notice: "Api messages running."
   end
 
